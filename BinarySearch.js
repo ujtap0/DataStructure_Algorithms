691cf6c9d7e97f474b5385c7d2f29d179d35384b
Trelawney <00uj@naver.com>
2022-06-30 14:02:57 +0900

    binarySearch 수정

--- BinarySearch.js
+++ BinarySearch.js
@@ -16,13 +16,13 @@ function binarySearch(arr, el){
   let left = 0;
   let right = arr.length-1;
   let mid = Math.floor((right+left)/2);
-  while(left < right){
+  while(left <= right && arr[mid] !== el){
     if(el < arr[mid]) right = mid - 1;
     if(el > arr[mid]) left = mid + 1;
-    if(el === arr[mid]) return mid
     mid = Math.floor((right+left)/2);
   }
+  if(arr[mid] === el) return mid;
   return -1
 }
 
-console.log(binarySearch([1,2,3,4,5,6,7,10,29,39,42,73], 100));
+console.log(binarySearch([1,2,3,4,5,6,7,10,29,39,42,73], 73));
