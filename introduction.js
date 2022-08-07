4279084ba96684c1a878eb6676aae2148c107dd9
Trelawney <00uj@naver.com>
2022-08-07 09:00:12 +0900

    single-liked-lists

---
+++ introduction.js
@@ -0,0 +1,22 @@
+// linked list: consist of a bunch of els with no indices (difference between array and linked list)
+// linked lists consist of nodes, and each node has a value and a pointer(reference) to another node or null
+// it contains a head, tail and length property
+// head: beginning of the linked list
+// tail: end of the linked list
+// SO WE DON'T KEEP TRACK OF EVERY SINGLE ITEM IN THE MIDDLE => WE JUST KEEP TRACK OF THE HEAD AND FROM THE HEAD WE CAN FIGURE OUT THE NEXT ONE
+// in array => every single item would have to be re-indexed to get a new position number
+
+// 리스트와 배열 비교
+//List
+// 1. dont have indices
+// 2. connected via nodes with a next pointer
+// 3. random access is not allowed
+
+//array
+// 1. indexed in order
+// 2. insertion and deletion can be expensive
+// 3. can quickly be accessed at a specific index
+
+//
+
+
